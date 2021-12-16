7096cb7920e9cd3713f448991f99aacb02811231
wangpengfei <2601339907@qq.com>
2021-12-16 15:13:19 +0800

    feat: add CubeState

--- src/rubiks/core/cube.ts
+++ src/rubiks/core/cube.ts
@@ -2,6 +2,7 @@ import {Camera, Color, Euler, Group, MathUtils, Matrix4, Vector, Vector2, Vector
 import {getAngleBetweenTwoVector2} from "../util/math";
 import {ndcToScreen} from "../util/transform";
 import CubeData from "./cubeData";
+import CubeState, {RotateDirection} from "./cubeState";
 import {createSquare, SquareMesh} from "./square";
 
 /**
@@ -19,6 +20,7 @@ const getTemPos = (square: SquareMesh, squareSize: number) => {
 
 export class Cube extends Group {
     private data: CubeData;
+    public state: CubeState;
     public get squares() {
         return this.children as SquareMesh[];
     }
@@ -33,6 +35,8 @@ export class Cube extends Group {
 
             this.add(square);
         }
+
+        this.state = new CubeState(this.squares);
     }
 
     public rotateOnePlane(mousePrePos: Vector2, mouseCurPos: Vector2, controlSquare: SquareMesh, camera: Camera, winSize: {w: number; h: number}) {
@@ -41,118 +45,126 @@ export class Cube extends Group {
         }
 
         const screenDir = mouseCurPos.clone().sub(mousePrePos);
-        const squareScreenPos = this.getSquareScreenPos(controlSquare, camera, winSize) as Vector2;
+        if (!this.state.inRotation) {
+            const squareScreenPos = this.getSquareScreenPos(controlSquare, camera, winSize) as Vector2;
 
-        const squareNormal = controlSquare.element.normal;
-        const squarePos = controlSquare.element.pos;
+            const squareNormal = controlSquare.element.normal;
+            const squarePos = controlSquare.element.pos;
 
-        // 与 controlSquare 在同一面的其他 Square
-        const commonDirSquares = this.squares.filter((square) => square.element.normal.equals(squareNormal) && !square.element.pos.equals(squarePos));
+            // 与 controlSquare 在同一面的其他 Square
+            const commonDirSquares = this.squares.filter((square) => square.element.normal.equals(squareNormal) && !square.element.pos.equals(squarePos));
 
-        // square1 和 sqaure2 垂直和竖直方向的同一面的两个 SquareMesh
-        let square1: SquareMesh | undefined;
-        let square2: SquareMesh | undefined;
-        for (let i = 0; i < commonDirSquares.length; i++) {
-            if (squareNormal.x !== 0) {
-                if (commonDirSquares[i].element.pos.y === squarePos.y) {
-                    square1 = commonDirSquares[i];
+            // square1 和 sqaure2 垂直和竖直方向的同一面的两个 SquareMesh
+            let square1: SquareMesh | undefined;
+            let square2: SquareMesh | undefined;
+            for (let i = 0; i < commonDirSquares.length; i++) {
+                if (squareNormal.x !== 0) {
+                    if (commonDirSquares[i].element.pos.y === squarePos.y) {
+                        square1 = commonDirSquares[i];
+                    }
+                    if (commonDirSquares[i].element.pos.z === squarePos.z) {
+                        square2 = commonDirSquares[i];
+                    }
+                } else if (squareNormal.y !== 0) {
+                    if (commonDirSquares[i].element.pos.x === squarePos.x) {
+                        square1 = commonDirSquares[i];
+                    }
+                    if (commonDirSquares[i].element.pos.z === squarePos.z) {
+                        square2 = commonDirSquares[i];
+                    }
+                } else if (squareNormal.z !== 0) {
+                    if (commonDirSquares[i].element.pos.x === squarePos.x) {
+                        square1 = commonDirSquares[i];
+                    }
+                    if (commonDirSquares[i].element.pos.y === squarePos.y) {
+                        square2 = commonDirSquares[i];
+                    }
                 }
-                if (commonDirSquares[i].element.pos.z === squarePos.z) {
-                    square2 = commonDirSquares[i];
-                }
-            } else if (squareNormal.y !== 0) {
-                if (commonDirSquares[i].element.pos.x === squarePos.x) {
-                    square1 = commonDirSquares[i];
-                }
-                if (commonDirSquares[i].element.pos.z === squarePos.z) {
-                    square2 = commonDirSquares[i];
-                }
-            } else if (squareNormal.z !== 0) {
-                if (commonDirSquares[i].element.pos.x === squarePos.x) {
-                    square1 = commonDirSquares[i];
-                }
-                if (commonDirSquares[i].element.pos.y === squarePos.y) {
-                    square2 = commonDirSquares[i];
+
+                if (square1 && square2) {
+                    break;
                 }
             }
 
-            if (square1 && square2) {
-                break;
+            if (!square1 || !square2) {
+                return;
             }
-        }
 
-        if (!square1 || !square2) {
-            return;
-        }
+            const square1ScreenPos = this.getSquareScreenPos(square1, camera, winSize) as Vector2;
+            const square2ScreenPos = this.getSquareScreenPos(square2, camera, winSize) as Vector2;
 
-        const square1ScreenPos = this.getSquareScreenPos(square1, camera, winSize) as Vector2;
-        const square2ScreenPos = this.getSquareScreenPos(square2, camera, winSize) as Vector2;
+            // 记录可能旋转的四个方向
+            const squareDirs: RotateDirection[] = [];
 
-        // 记录可能旋转的四个方向
-        const squareDirs: {
-            screenDir: Vector2; // 屏幕方向向量
-            startSquare: SquareMesh; // 代表方向的起始square，用于记录旋转的local方向
-            endSquare: SquareMesh; // 代表方向的终止square，用于记录旋转的local方向
-        }[] = [];
+            const squareDir1 = {
+                screenDir: new Vector2(square1ScreenPos.x - squareScreenPos.x, square1ScreenPos.y - squareScreenPos.y).normalize(),
+                startSquare: controlSquare,
+                endSquare: square1
+            };
+            const squareDir2 = {
+                screenDir: new Vector2(square2ScreenPos.x - squareScreenPos.x, square2ScreenPos.y - squareScreenPos.y).normalize(),
+                startSquare: controlSquare,
+                endSquare: square2
+            };
+            squareDirs.push(squareDir1);
+            squareDirs.push({
+                screenDir: squareDir1.screenDir.clone().negate(),
+                startSquare: square1,
+                endSquare: controlSquare
+            });
+            squareDirs.push(squareDir2);
+            squareDirs.push({
+                screenDir: squareDir2.screenDir.clone().negate(),
+                startSquare: square2,
+                endSquare: controlSquare
+            });
 
-        const squareDir1 = {
-            screenDir: new Vector2(square1ScreenPos.x - squareScreenPos.x, square1ScreenPos.y - squareScreenPos.y).normalize(),
-            startSquare: controlSquare,
-            endSquare: square1
-        };
-        const squareDir2 = {
-            screenDir: new Vector2(square2ScreenPos.x - squareScreenPos.x, square2ScreenPos.y - squareScreenPos.y).normalize(),
-            startSquare: controlSquare,
-            endSquare: square2
-        };
-        squareDirs.push(squareDir1);
-        squareDirs.push({
-            screenDir: squareDir1.screenDir.clone().negate(),
-            startSquare: square1,
-            endSquare: controlSquare
-        });
-        squareDirs.push(squareDir2);
-        squareDirs.push({
-            screenDir: squareDir2.screenDir.clone().negate(),
-            startSquare: square2,
-            endSquare: controlSquare
-        });
+            // 根据可能旋转的四个方向向量与鼠标平移方向的夹角确定旋转的方向，夹角最小的方向即为旋转方向
+            let minAngle = Math.abs(getAngleBetweenTwoVector2(squareDirs[0].screenDir, screenDir));
+            let rotateDir = squareDirs[0];  // 最终确定的旋转方向
 
-        // 根据可能旋转的四个方向向量与鼠标平移方向的夹角确定旋转的方向，夹角最小的方向即为旋转方向
-        let minAngle = Math.abs(getAngleBetweenTwoVector2(squareDirs[0].screenDir, screenDir));
-        let rotateDir = squareDirs[0];  // 最终确定的旋转方向
+            for (let i = 0; i < squareDirs.length; i++) {
+                const angle = Math.abs(getAngleBetweenTwoVector2(squareDirs[i].screenDir, screenDir));
 
-        for (let i = 0; i < squareDirs.length; i++) {
-            const angle = Math.abs(getAngleBetweenTwoVector2(squareDirs[i].screenDir, screenDir));
-
-            if (minAngle > angle) {
-                minAngle = angle;
-                rotateDir = squareDirs[i];
+                if (minAngle > angle) {
+                    minAngle = angle;
+                    rotateDir = squareDirs[i];
+                }
             }
+
+            // 旋转轴：用法向量与旋转的方向的叉积计算
+            const rotateDirLocal = rotateDir.endSquare.element.pos.clone().sub(rotateDir.startSquare.element.pos).normalize();
+            const rotateAxisLocal = squareNormal.clone().cross(rotateDirLocal).normalize(); // 旋转的轴
+
+            // 旋转的角度：使用 screenDir 在旋转方向上的投影长度，投影长度越长，旋转角度越大
+            const screenDirProjectRotateDirLen = Math.cos(minAngle) * screenDir.length();
+            const rotateAnglePI = screenDirProjectRotateDirLen / 1000 * Math.PI; // 旋转角度
+
+            // 旋转的方块：由 controlSquare 位置到要旋转的方块的位置的向量，与旋转的轴是垂直的，通过这一特性可以筛选出所有要旋转的方块
+            const rotateSquares: SquareMesh[] = [];
+            const controlTemPos = getTemPos(controlSquare, this.data.elementSize);
+
+            for (let i = 0; i < this.squares.length; i++) {
+                const squareTemPos = getTemPos(this.squares[i], this.data.elementSize);
+                const squareVec = controlTemPos.clone().sub(squareTemPos);
+                if (squareVec.dot(rotateAxisLocal) === 0) {
+                    rotateSquares.push(this.squares[i]);
+                }
+            }
+
+            this.state.setRotating(controlSquare, rotateSquares, rotateDir, rotateAxisLocal);
         }
 
-        // 旋转轴：用法向量与旋转的方向的叉积计算
-        const rotateDirLocal = rotateDir.endSquare.element.pos.clone().sub(rotateDir.startSquare.element.pos).normalize();
-        const rotateAxisLocal = squareNormal.clone().cross(rotateDirLocal).normalize(); // 旋转的轴
+        const rotateSquares = this.state.activeSquares; // 旋转的方块
+        const rotateAxisLocal = this.state.rotateAxisLocal; // 旋转的轴
 
         // 旋转的角度：使用 screenDir 在旋转方向上的投影长度，投影长度越长，旋转角度越大
-        const screenDirProjectRotateDirLen = Math.cos(minAngle) * screenDir.length();
+        const temAngle = Math.abs(getAngleBetweenTwoVector2(this.state.rotateDirection!.screenDir, screenDir));
+        const screenDirProjectRotateDirLen = Math.cos(temAngle) * screenDir.length();
         const rotateAnglePI = screenDirProjectRotateDirLen / 1000 * Math.PI; // 旋转角度
 
-        // 旋转的方块：由 controlSquare 位置到要旋转的方块的位置的向量，与旋转的轴是垂直的，通过这一特性可以筛选出所有要旋转的方块
-        const rotateSquares: SquareMesh[] = [];
-        const controlTemPos = getTemPos(controlSquare, this.data.elementSize);
-
-        for (let i = 0; i < this.squares.length; i++) {
-            const squareTemPos = getTemPos(this.squares[i], this.data.elementSize);
-            const squareVec = controlTemPos.clone().sub(squareTemPos);
-            if (squareVec.dot(rotateAxisLocal) === 0) {
-                rotateSquares.push(this.squares[i]);
-            }
-        }
-
         const rotateMat = new Matrix4();
-        rotateMat.makeRotationAxis(rotateAxisLocal, rotateAnglePI);
+        rotateMat.makeRotationAxis(rotateAxisLocal!, rotateAnglePI);
 
         for (let i = 0; i < rotateSquares.length; i++) {
             rotateSquares[i].applyMatrix4(rotateMat);
---
+++ src/rubiks/core/cubeState.ts
@@ -0,0 +1,35 @@
+import {Vector3, Vector2} from "three";
+import {SquareMesh} from "./square";
+
+export interface RotateDirection {
+    screenDir: Vector2; // 屏幕方向向量
+    startSquare: SquareMesh; // 代表方向的起始square，用于记录旋转的local方向
+    endSquare: SquareMesh; // 代表方向的终止square，用于记录旋转的local方向
+}
+
+class CubeState {
+    /** 所有方块 */
+    private _squares: SquareMesh[];
+    /** 是否正处于旋转状态 */
+    public inRotation = false;
+    /** 正在旋转的方块 */
+    public activeSquares: SquareMesh[] = [];
+    /** 控制的方块 */
+    public controlSquare: SquareMesh | undefined;
+    /** 旋转方向 */
+    public rotateDirection: RotateDirection | undefined;
+    /** 旋转轴 */
+    public rotateAxisLocal: Vector3 | undefined;
+    public constructor(squares: SquareMesh[]) {
+        this._squares = squares;
+    }
+
+    public setRotating(control: SquareMesh, actives: SquareMesh[], direction: RotateDirection, rotateAxisLocal: Vector3) {
+        this.controlSquare = control;
+        this.activeSquares = actives;
+        this.rotateDirection = direction;
+        this.rotateAxisLocal = rotateAxisLocal;
+    }
+}
+
+export default CubeState;
